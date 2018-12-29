840eb7ba87e7b890e5c474b46d30fa81bd7edc09
我爱Github <taoste@gmail.com>
2018-12-29 15:23:00 +0800

    （ 命令: copy /y 方正兰亭黑长.ttf %windir%\fonts ）

---
+++ Tools/字体（fonts）/方正兰亭黑体(全套字体包)/安装字体.bat
@@ -0,0 +1,30 @@
+copy /y 兰亭粗黑 GBK.TTF %windir%\fonts
+copy /y 兰亭黑简.TTF %windir%\fonts
+copy /y 兰亭特黑长简 GBK.TTF %windir%\fonts
+copy /y 兰亭特黑简.TTF %windir%\fonts
+copy /y 兰亭粗黑简.TTF %windir%\fonts
+copy /y 兰亭特黑扁简 GBK.TTF %windir%\fonts
+copy /y 兰亭特黑长简.TTF %windir%\fonts
+copy /y 兰亭细黑 GBK Mobil.TTF %windir%\fonts
+copy /y 兰亭黑 GBK.TTF %windir%\fonts
+copy /y 兰亭特黑扁简.TTF %windir%\fonts
+copy /y 兰亭特黑简 GBK.TTF %windir%\fonts
+copy /y 兰亭细黑 GBK.TTF %windir%\fonts
+copy /y 方正兰亭中粗黑_GBK.ttf %windir%\fonts
+copy /y 方正兰亭中黑_GBK.TTF %windir%\fonts
+copy /y 方正兰亭准黑_GBK.TTF %windir%\fonts
+copy /y 方正兰亭准黑简体.TTF %windir%\fonts
+copy /y 方正兰亭刊宋_GBK.TTF %windir%\fonts
+copy /y 方正兰亭刊黑_GBK.ttf %windir%\fonts
+copy /y 方正兰亭大黑_GBK.ttf %windir%\fonts
+copy /y 方正兰亭宋.ttf %windir%\fonts
+copy /y 方正兰亭特黑.TTF %windir%\fonts
+copy /y 方正兰亭特黑扁.TTF %windir%\fonts
+copy /y 方正兰亭特黑长.TTF %windir%\fonts
+copy /y 方正兰亭粗黑.TTF %windir%\fonts
+copy /y 方正兰亭纤黑.ttf %windir%\fonts
+copy /y 方正兰亭细黑_GBK.TTF %windir%\fonts
+copy /y 方正兰亭超细黑.TTF %windir%\fonts
+copy /y 方正兰亭黑.TTF %windir%\fonts
+copy /y 方正兰亭黑扁.ttf %windir%\fonts
+copy /y 方正兰亭黑长.ttf %windir%\fonts
