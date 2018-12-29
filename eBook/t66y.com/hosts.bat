54c541716941139381df86d7066927ba3f2991cb
我爱Github <taoste@gmail.com>
2018-12-29 14:07:34 +0800

    Update hosts.bat

--- eBook/t66y.com/hosts.bat
+++ eBook/t66y.com/hosts.bat
@@ -10,6 +10,9 @@ echo "请注意你的杀毒软件提示，一定要允许"
 @echo 104.25.31.112 t66y.com >>C:\Windows\System32\drivers\etc\hosts
 @echo 2400:CB00:2048:1::6819:1F70 t66y.com >>C:\Windows\System32\drivers\etc\hosts
 @echo 2400:CB00:2048:1::6819:2070 t66y.com >>C:\Windows\System32\drivers\etc\hosts
+@echo #sex.com  >>C:\Windows\System32\drivers\etc\hosts
+@echo 206.125.164.82 sex.com  >>C:\Windows\System32\drivers\etc\hosts
+@echo 2607:FC10:1:400::82 sex.com  >>C:\Windows\System32\drivers\etc\hosts
 @echo #佐拉 - 周曙光的个人网站 >>C:\Windows\System32\drivers\etc\hosts
 @echo 104.28.27.52 zuola.com >>C:\Windows\System32\drivers\etc\hosts
 @echo 104.28.26.52 zuola.com >>C:\Windows\System32\drivers\etc\hosts
