b0ce16cc8fa8a8d51d07f94b859dc26f627e9275
我爱Github <taoste@gmail.com>
2018-12-30 15:56:34 +0800

    Add files via upload

---
+++ github/12306/12306Bypass/12306bypass.com/Download.aspx
@@ -0,0 +1,30 @@
+﻿
+
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><title>
+
+</title>
+    <style type="text/css">
+        #Radio1 {
+            height: 20px;
+        }
+    </style>
+</head>
+<body>
+    <form method="post" action="./Download.aspx" id="form1">
+<div class="aspNetHidden">
+<input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="/wEPDwUKLTEzNDM3NzkxOWRkwloRYaMuTLw5m4udsMa+ir/t/09rB6vo92nhilu3NZc=" />
+</div>
+
+<div class="aspNetHidden">
+
+	<input type="hidden" name="__VIEWSTATEGENERATOR" id="__VIEWSTATEGENERATOR" value="E0C68A58" />
+</div>
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ github/12306/12306Bypass/12306bypass.com/js/index.js
@@ -0,0 +1,69 @@
+﻿$(function () {
+    $('#layerslider').layerSlider({
+        skinsPath: 'layerslider/skins/',
+        animateFirstLayer: true,
+        navPrevNext: true,
+        navStartStop: false,
+        navButtons: false,
+        autoPlayVideos: false,
+        skin: 'minimal'
+    });
+    getUpdate();
+    setHits();
+    var interval_output = setInterval("setHits()", 60000);
+    try {
+        var hiddenProperty = 'hidden' in document ? 'hidden' :
+            'webkitHidden' in document ? 'webkitHidden' :
+            'mozHidden' in document ? 'mozHidden' :
+            null;
+        var visibilityChangeEvent = hiddenProperty.replace(/hidden/i, 'visibilitychange');
+        var onVisibilityChange = function () {
+            if (!document[hiddenProperty]) {
+                setHits();
+                interval_output = setInterval("setHits()", 60000);
+            } else {
+                clearInterval(interval_output);
+            }
+        }
+        document.addEventListener(visibilityChangeEvent, onVisibilityChange);
+    } catch (e) {
+        setInterval("setHits()", 60000);
+    }
+});
+$(window).load(function () {
+    changeFooter();
+    window.onresize = function () {
+        var res;
+        if (res) { clearTimeout(res) }
+        res = setTimeout(changeFooter(), 20);
+    };
+});
+
+function setHits() {
+    $.post('GetList.ashx', {
+        _rid: Math.random()
+    },
+    function (str) {
+        $('#dynamicInfo').html(str);
+    });
+}
+function getUpdate() {
+    $.post('log.aspx', {
+        visitType: 1
+    },
+    function (data) {
+        $('#version').html(data.version);
+        $('#updatedate').html(data.updatedate);
+        $('#datasize').html(data.datasize);
+    },
+    'json')
+}
+function changeFooter() {
+    $('.bottom').css('min-height', 0);
+    var rangeIndex = document.documentElement.clientHeight - $('#footer').offset().top - $('#footer').height();
+    if (rangeIndex > 0) {
+        $('.bottom').css('min-height', rangeIndex);
+    } else {
+        $('.bottom').css('min-height', 0);
+    }
+}
