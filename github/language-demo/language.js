d9a6e3412e293d4d9999a83950dcba8a0cad9434
我爱Github <taoste@gmail.com>
2019-01-09 11:04:30 +0800

    HTML多语言切换，Jquery实现
    Html多语言切换，Jquery实现 - yjing blog
    https://yjgit.github.io/2017/08/10/multiple-language-html/

---
+++ github/language-demo/language.js
@@ -0,0 +1,18 @@
+﻿$(function(){
+     //语言默认英文
+     var language = $('#lang :selected').val();
+     $("span:lang(ch)").hide();
+     $("span:lang(en)").show();
+     //用语言种类选择
+     $('#lang').change(function(){
+         language = $('#lang :selected').val();
+         if(language == "en"){
+             $("span:lang(ch)").hide();
+             $("span:lang(en)").show();
+         }
+         else{
+             $("span:lang(en)").hide();
+             $("span:lang(ch)").show();
+         }
+     })
+ })
