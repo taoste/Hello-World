6cb03aecc92ca54963f280d6aac83dbee26b10f0
我爱Github <taoste@gmail.com>
2019-07-27 16:39:43 +0800

    将必应Bing每日图片设置为网站背景
    如何将必应Bing每日图片设置为网站背景 - 范明明
    （ https://www.fanmingming.com/article/bing-images-bg.html  ）

---
+++ github/bg/1.php
@@ -0,0 +1,16 @@
+﻿<?php
+    $str=file_get_contents('https://cn.bing.com/HPImageArchive.aspx?idx=1&n=1');
+if (preg_match("/<url>(.+?)<\/url>/ies", $str, $matches)) {
+    $imgurl='https://cn.bing.com'.$matches[1];
+}
+if ($imgurl) {
+    header('Content-Type: image/JPEG');
+    @ob_end_clean();
+    @readfile($imgurl);
+    @flush();
+    @ob_flush();
+    exit();
+} else {
+    exit('error');
+}
+?>
---
+++ github/bg/bg.php
@@ -0,0 +1,11 @@
+﻿<?php
+    $str=file_get_contents('https://cn.bing.com/HPImageArchive.aspx?idx=0&n=1');
+if (preg_match("/<url>(.+?)<\/url>/ies", $str, $matches)) {
+    $imgurl='https://cn.bing.com'.$matches[1];
+}
+if ($imgurl) {
+    header("Location: $imgurl");    //输出302跳转
+} else {
+    exit('error');
+}
+?>
