579e09ed01cd3eda5b35bf15852f181a365373fc
我爱Github <9572479+taoste@users.noreply.github.com>
2020-07-22 14:19:42 +0800

    Create down.bat

---
+++ Music/吴晓波：历代经济变革得失/down.bat
@@ -0,0 +1,17 @@
+wget http://audio.xmcdn.com/group4/M0A/82/38/wKgDs1RJ7YGBedd8AYZQzfKQhvE970.mp3 1_吴晓波：历代经济变革得失1：研究中国的方法.mp3
+wget http://audio.xmcdn.com/group4/M0A/82/38/wKgDs1RJ7bPxbHOLAWJRSUJsonI653.mp3 2_吴晓波：历代经济变革得失2管仲变法：中国式改革思想的形成上.mp3
+wget http://audio.xmcdn.com/group4/M08/82/38/wKgDs1RJ7eLzaLyGAUNMDfr-BuU109.mp3 3_吴晓波：历代经济变革得失3商鞅变法：中国式改革思想的形成下.mp3
+wget http://audio.xmcdn.com/group4/M08/85/A6/wKgDtFRJ7fyDFad6AR7GqhVEDU0377.mp3 4_吴晓波：历代经济变革得失4汉武帝变法.mp3
+wget http://audio.xmcdn.com/group4/M09/85/A7/wKgDtFRJ7knjef_HAaGtsk8S28o155.mp3 5_吴晓波：历代经济变革得失5王莽变法.mp3
+wget http://aod.tx.xmcdn.com/group4/M07/85/A8/wKgDtFRJ7rvzj8ShAYeLCzCVQRc102.mp3 6_吴晓波：历代经济变革得失6刘晏变法.mp3
+wget http://audio.xmcdn.com/group4/M07/85/A8/wKgDtFRJ7sHgzWSJAW5nH6zttGw864.mp3 7_吴晓波：历代经济变革得失7王安石变法.mp3
+wget http://audio.xmcdn.com/group4/M08/85/A9/wKgDtFRJ7w6gNJ_aAbx6EMmVMvM291.mp3 8_吴晓波：历代经济变革得失8明清停滞.mp3
+wget http://audio.xmcdn.com/group4/M0A/82/3B/wKgDs1RJ70XS123qAaaML2xyc_o814.mp3 9_吴晓波：历代经济变革得失9李鸿章变法.mp3
+wget http://audio.xmcdn.com/group4/M0A/85/A9/wKgDtFRJ72CQihS-AUiaa3pxKsw218.mp3 10_吴晓波：历代经济变革得失10孔宋变法.mp3
+wget http://audio.xmcdn.com/group4/M09/85/AB/wKgDtFRJ7_exVU8PAZRIRLHBPnc261.mp3 11_吴晓波：历代经济变革得失11陈云变法.mp3
+wget http://audio.xmcdn.com/group4/M07/82/3D/wKgDs1RJ7_LgXvRgAcF-MKxXD2o170.mp3 12_吴晓波：历代经济变革得失12改革开放.mp3
+
+
+wget .bat - 国内版 Bing https://cn.bing.com/search?q=wget+++.bat
+
+wget -x http://fdfs.xmcdn.com/group30/M03/5A/E3/wKgJXlmLpquSIxJzACeaPraX5vs278.mp3 > 278.mp3
